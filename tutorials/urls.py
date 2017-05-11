ac2c83f066646877077c200c5b38f7c4d38f4309
ChaceMcGuyer <mcguyerchace@gmail.com>
2017-05-10 17:30:34 -0700

    fixed url regex issue

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.tutorial_landing, name='tutorial_landing',),
+    url(r'^', views.tutorial_landing, name='tutorial_landing',),
     url(r'(?P<pk>\d+)/$', views.tutorial_detail, name='tutorial_detail'),
     url(r'new/$', views.tutorial_new, name='tutorial_new'),
     url(r'(?P<pk>\d+)/edit/$', views.tutorial_edit, name='tutorial_edit'),
