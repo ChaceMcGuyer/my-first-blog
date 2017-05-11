ce4cae3687b7c860fa9bdc5b5493c882b7591e5e
ChaceMcGuyer <mcguyerchace@gmail.com>
2017-05-10 19:08:15 -0700

    fixed a url issue

--- code_games/urls.py
+++ code_games/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.game_list, name='game_list'),
+    url(r'^$', views.game_list, name='game_list'),
     url(r'(?P<pk>\d+)/$', views.game_single, name='game_single'),
     url(r'^new/$', views.game_new, name='game_new'),
 ]
